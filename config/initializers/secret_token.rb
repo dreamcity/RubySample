3f28f74d56c8842a285604529e391b6583a031f7
dreamcity <dreamscity.com>
2013-07-27 11:22:24 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '2bd06a5ddb3db4c8cfff1306d77fef3bd658268f5a9804a67c726eedd3961caa9254dc91c538105ac2ff4b9ff712241e44944609f8becabab4c142c4f64c657f'
