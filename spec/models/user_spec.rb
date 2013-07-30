3315279ba6a41e8c2aab9b24ed7cb600e585fdd8
dreamcity <dreamscity.com>
2013-07-30 11:52:54 +0800

    valid length&format

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,21 @@ describe User do
   	before{ @user.name=""}
   	it { should_not be_valid}
   end
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+  describe "when email is not present" do
+  	before{ @user.email=" "}
+  	it { should_not be_valid}
+  end
+    describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+  end
+end
 end
