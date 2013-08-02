187b0df007bacd67f758e3b982ef0c9598726ce8
dreamcity <dreamscity.com>
2013-08-02 15:25:21 +0800

    list users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [ :index, :edit, :update]
+  before_action :correct_user,   only: [ :edit, :update]
+  def index
+  	@users = User.all
+  end
   def show
   	@user = User.find(params[:id])
   end
@@ -7,8 +12,10 @@ class UsersController < ApplicationController
   	@user = User.new
   end
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
+
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -19,11 +26,32 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    elsif 
+      render 'edit'
+    end
+  end
 
   private
 
 	  def user_params
-	      params.require(:user).permit(:name, :email, :password,
-	                                   :password_confirmation)
+	      params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	  end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in "
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
